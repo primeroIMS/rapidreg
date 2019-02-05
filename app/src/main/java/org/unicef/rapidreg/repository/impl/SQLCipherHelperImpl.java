e8ab787ee90760808297a5dc5340f9568d3caeec
Joshua Toliver <jtoliver@quoininc.com>
2019-02-05 17:22:43 -0500

    Removing phone state permission and function call that uses it.

--- app/src/main/java/org/unicef/rapidreg/repository/impl/SQLCipherHelperImpl.java
+++ app/src/main/java/org/unicef/rapidreg/repository/impl/SQLCipherHelperImpl.java
@@ -1,8 +1,12 @@
 package org.unicef.rapidreg.repository.impl;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
 
 import com.raizlabs.android.dbflow.config.DatabaseDefinition;
@@ -12,6 +16,8 @@ import com.raizlabs.dbflow.android.sqlcipher.SQLCipherOpenHelper;
 import org.unicef.rapidreg.BuildConfig;
 import org.unicef.rapidreg.PrimeroApplication;
 import org.unicef.rapidreg.utils.EncryptHelper;
+import android.provider.Settings.Secure;
+
 
 import org.unicef.rapidreg.utils.KeyStoreUtils;
 
@@ -39,17 +45,15 @@ public class SQLCipherHelperImpl extends SQLCipherOpenHelper {
 
     private String generateEncryptionKey() {
         Context ctx = PrimeroApplication.getAppContext();
+
         String androidId = Settings.Secure.getString(ctx.getContentResolver(),
                 Settings.Secure.ANDROID_ID);
-
-        TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
-        String deviceId = tm.getDeviceId();
 
         String buildInfo = Build.BOARD + Build.BRAND + Build.DEVICE + Build.DISPLAY
                 + Build.FINGERPRINT + Build.HOST + Build.ID + Build.MANUFACTURER + Build.MODEL
                 + Build.PRODUCT + Build.TAGS + Build.TYPE + Build.USER;
 
-        return EncryptHelper.encrypt(buildInfo + deviceId + androidId);
+        return EncryptHelper.encrypt(buildInfo + androidId);
     }
 
 }
